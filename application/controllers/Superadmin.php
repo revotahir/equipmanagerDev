39c117c8ae0cc1f968ff468592f713c35ae3fb83
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2025-11-13 02:36:48 +0530

    Manage Category

---
+++ application/controllers/Superadmin.php
@@ -0,0 +1,81 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class superadmin extends MY_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('file');
+        $this->load->model('Generic_model', 'generic');
+    }
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/userguide3/general/urls.html
+     */
+
+
+    // super admin login
+    public function superadmindashboard()
+    {
+        //check if user is vendor
+        if ($this->session->userdata('loginData')['userType'] == 1) {
+            $this->load->view('superadmin/superadmindashboard');
+        } else {
+            redirect(base_url());
+        }
+    }
+
+
+    // manage super category
+    public function ManageSuperCategory()
+    {
+        $this->load->view('superadmin/managecategory');
+    }
+
+    public function addSuperCategory()
+    {
+        $superCatName = $this->input->post('catName');
+        $superCatDesp = $this->input->post('catDesc');
+
+        //check if we have image posted
+        if (empty($_FILES['catIcon']['name'])) {
+            $personImage = 'category.svg';
+        } else {
+            //upload person image
+            $config['upload_path']          = './assets/uploads/superadmin/category';
+            $config['allowed_types']        = 'svg';
+            $config['max_size']             = 1024;
+            $config['encrypt_name']         = TRUE;
+            $this->load->library('upload', $config);
+            if (! $this->upload->do_upload('catIcon')) {
+                $error = array('error' => $this->upload->display_errors());
+                // die(print_r($error));
+                $this->session->set_flashdata('error', $error['error']);
+                redirect(base_url('manage-super-category'));
+            } else {
+                $uploadData = $this->upload->data();
+                $personImage = $uploadData['file_name'];
+            }
+        }
+        $data = array(
+            'web_catName' => $superCatName,
+            'web_catDesp' => $superCatDesp,
+            'web_catIcon' => $personImage,
+            'web_catStatus' => 1,
+        );
+        $this->generic->InsertData('web_cat', $data);
+    }
+}
--- application/views/components/sidemenu.php
+++ application/views/components/sidemenu.php
@@ -1,97 +1,212 @@
-  <!--start sidebar -->
-  <aside class="sidebar-wrapper" data-simplebar="true">
-      <div class="sidebar-header">
-          <div>
-              <img
-                  src="<?= base_url() ?>assets/images/logo-icon.png"
-                  class="logo-icon"
-                  alt="logo icon" />
+  <?php
+    if ($this->session->userdata('loginData')['userType'] == 1) {
+    ?>
+
+      <!--start sidebar -->
+      <aside class="sidebar-wrapper" data-simplebar="true">
+          <div class="sidebar-header">
+              <div>
+                  <img
+                      src="<?= base_url() ?>assets/images/logo-icon.png"
+                      class="logo-icon"
+                      alt="logo icon" />
+              </div>
+              <div>
+                  <h4 class="logo-text" style="color: black">Equip Manager</h4>
+              </div>
+              <div class="toggle-icon ms-auto">
+                  <i class="bi bi-chevron-double-left" style="color: black"></i>
+              </div>
           </div>
-          <div>
-              <h4 class="logo-text" style="color: black">Equip Manager</h4>
-          </div>
-          <div class="toggle-icon ms-auto">
-              <i class="bi bi-chevron-double-left" style="color: black"></i>
-          </div>
-      </div>
-      <!--navigation-->
-      <ul class="metismenu" id="menu">
-          <li>
-              <?php
-            //   dashboard for company vendor 
-                if ($this->session->userdata('loginData')['userType'] == 2) {
-                ?>
-                  <a href="<?=base_url('company-dashboard')?>">
+          <!--navigation-->
+          <ul class="metismenu" id="menu">
+              <li>
+                  <a href="<?= base_url('admin-dashboard') ?>">
                       <div class="parent-icon"><i class="bi bi-house-door"></i></div>
                       <div class="menu-title font-clash">Dashboard</div>
                   </a>
-              <?php
-                }
-                ?>
-          </li>
-          <li>
-              <a href="javascript:;" class="has-arrow">
-                  <div class="parent-icon"><i class="bi bi-people-fill"></i></div>
-                  <div class="menu-title font-clash">Manage Workforce</div>
-              </a>
-              <ul>
-                  <li>
-                      <a href="<?=base_url('add-workforce')?>"><i class="bi bi-arrow-right-short"></i>Add workforce</a>
-                  </li>
-                  <li>
-                      <a href="<?=base_url('all-workforce')?>"><i class="bi bi-arrow-right-short"></i>All Workforce</a>
-                  </li>
-                  <li>
-                      <a href="<?=base_url('manage-workforce-skill')?>"><i class="bi bi-arrow-right-short"></i>Manage Skills</a>
-                  </li>
-              </ul>
-          </li>
+              </li>
+              <li>
+                  <a href="javascript:;" class="has-arrow">
+                      <div class="parent-icon"><i class="bi bi-people-fill"></i></div>
+                      <div class="menu-title font-clash">Manage Category</div>
+                  </a>
+                  <ul>
+                      <li>
+                          <a href="<?= base_url('manage-super-category') ?>"><i class="bi bi-arrow-right-short"></i>Add Category</a>
+                      </li>
+                  </ul>
+              </li>
 
-          <!-- manage Equipment -->
-          <li>
-              <a class="has-arrow" href="javascript:;">
-                  <div class="parent-icon">
-                      <i class="bi bi-tools"></i>
-                  </div>
-                  <div class="menu-title font-clash">Manage Equipment</div>
-              </a>
-              <ul>
+              <!-- manage Testimonial -->
+              <li>
+                  <a class="has-arrow" href="javascript:;">
+                      <div class="parent-icon">
+                          <i class="bi bi-tools"></i>
+                      </div>
+                      <div class="menu-title font-clash">Manage Testimonial</div>
+                  </a>
+                  <ul>
 
-                  <li>
-                      <a href="<?=base_url('add-equipment')?>"><i class="bi bi-arrow-right-short"></i>Add Equipment</a>
-                  </li>
-                  <li>
-                      <a href="<?=base_url('all-equipment')?>"><i class="bi bi-arrow-right-short"></i>All Equipment </a>
-                  </li>
-                  <li>
-                      <a href="<?=base_url('manage-category')?>"><i class="bi bi-arrow-right-short"></i>Manage Category</a>
-                  </li>
-              </ul>
-          </li>
-          <!-- mannage  Equipment end here-->
-          <!-- manage Project -->
-          <li>
-              <a class="has-arrow" href="javascript:;">
-                  <div class="parent-icon">
-                      <i class="bx bx-window-alt"></i>
-                  </div>
-                  <div class="menu-title font-clash">Manage Projects</div>
-              </a>
-              <ul>
+                      <li>
+                          <a href="<?= base_url('') ?>"><i class="bi bi-arrow-right-short"></i>Add Testimonial</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- mannage  Testimonial end here-->
+              <!-- manage Blog -->
+              <li>
+                  <a class="has-arrow" href="javascript:;">
+                      <div class="parent-icon">
+                          <i class="bx bx-window-alt"></i>
+                      </div>
+                      <div class="menu-title font-clash">Manage Blog</div>
+                  </a>
+                  <ul>
 
-                  <li>
-                      <a href="<?=base_url('add-project')?>"><i class="bi bi-arrow-right-short"></i>Add Project</a>
-                  </li>
-                  <li>
-                      <a href="<?=base_url('all-projects')?>"><i class="bi bi-arrow-right-short"></i>All Projects </a>
-                  </li>
-                  <li>
-                      <a href="<?=base_url('project-category')?>"><i class="bi bi-arrow-right-short"></i>Project Category</a>
-                  </li>
-              </ul>
-          </li>
-          <!-- mannage  Equipment end here-->
-      </ul>
-      <!--end navigation-->
-  </aside>
-  <!--end sidebar -->
+                      <li>
+                          <a href="<?= base_url('') ?>"><i class="bi bi-arrow-right-short"></i>Add Blog</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- mannage  Blog end here-->
+              <!-- manage Success Metrics -->
+              <li>
+                  <a class="has-arrow" href="javascript:;">
+                      <div class="parent-icon">
+                          <i class="bx bx-window-alt"></i>
+                      </div>
+                      <div class="menu-title font-clash">Manage Metrics</div>
+                  </a>
+                  <ul>
+
+                      <li>
+                          <a href="<?= base_url('') ?>"><i class="bi bi-arrow-right-short"></i>Add Success Metrics</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- mannage  Success Metrics end here-->
+              <!-- manage Company Partners -->
+              <li>
+                  <a class="has-arrow" href="javascript:;">
+                      <div class="parent-icon">
+                          <i class="bx bx-window-alt"></i>
+                      </div>
+                      <div class="menu-title font-clash">Manage Company Partners
+                      </div>
+                  </a>
+                  <ul>
+
+                      <li>
+                          <a href="<?= base_url('') ?>"><i class="bi bi-arrow-right-short"></i>Add Company Partners</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- mannage  Company Partners end here-->
+          </ul>
+          <!--end navigation-->
+      </aside>
+      <!--end sidebar -->
+  <?php
+    } else {
+    ?>
+      <!--start sidebar -->
+      <aside class="sidebar-wrapper" data-simplebar="true">
+          <div class="sidebar-header">
+              <div>
+                  <img
+                      src="<?= base_url() ?>assets/images/logo-icon.png"
+                      class="logo-icon"
+                      alt="logo icon" />
+              </div>
+              <div>
+                  <h4 class="logo-text" style="color: black">Equip Manager</h4>
+              </div>
+              <div class="toggle-icon ms-auto">
+                  <i class="bi bi-chevron-double-left" style="color: black"></i>
+              </div>
+          </div>
+          <!--navigation-->
+          <ul class="metismenu" id="menu">
+              <li>
+                  <?php
+                    //   dashboard for company vendor 
+                    if ($this->session->userdata('loginData')['userType'] == 2) {
+                    ?>
+                      <a href="<?= base_url('company-dashboard') ?>">
+                          <div class="parent-icon"><i class="bi bi-house-door"></i></div>
+                          <div class="menu-title font-clash">Dashboard</div>
+                      </a>
+                  <?php
+                    }
+                    ?>
+              </li>
+              <li>
+                  <a href="javascript:;" class="has-arrow">
+                      <div class="parent-icon"><i class="bi bi-people-fill"></i></div>
+                      <div class="menu-title font-clash">Manage Workforce</div>
+                  </a>
+                  <ul>
+                      <li>
+                          <a href="<?= base_url('add-workforce') ?>"><i class="bi bi-arrow-right-short"></i>Add workforce</a>
+                      </li>
+                      <li>
+                          <a href="<?= base_url('all-workforce') ?>"><i class="bi bi-arrow-right-short"></i>All Workforce</a>
+                      </li>
+                      <li>
+                          <a href="<?= base_url('manage-workforce-skill') ?>"><i class="bi bi-arrow-right-short"></i>Manage Skills</a>
+                      </li>
+                  </ul>
+              </li>
+
+              <!-- manage Equipment -->
+              <li>
+                  <a class="has-arrow" href="javascript:;">
+                      <div class="parent-icon">
+                          <i class="bi bi-tools"></i>
+                      </div>
+                      <div class="menu-title font-clash">Manage Equipment</div>
+                  </a>
+                  <ul>
+
+                      <li>
+                          <a href="<?= base_url('add-equipment') ?>"><i class="bi bi-arrow-right-short"></i>Add Equipment</a>
+                      </li>
+                      <li>
+                          <a href="<?= base_url('all-equipment') ?>"><i class="bi bi-arrow-right-short"></i>All Equipment </a>
+                      </li>
+                      <li>
+                          <a href="<?= base_url('manage-category') ?>"><i class="bi bi-arrow-right-short"></i>Manage Category</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- mannage  Equipment end here-->
+              <!-- manage Project -->
+              <li>
+                  <a class="has-arrow" href="javascript:;">
+                      <div class="parent-icon">
+                          <i class="bx bx-window-alt"></i>
+                      </div>
+                      <div class="menu-title font-clash">Manage Projects</div>
+                  </a>
+                  <ul>
+
+                      <li>
+                          <a href="<?= base_url('add-project') ?>"><i class="bi bi-arrow-right-short"></i>Add Project</a>
+                      </li>
+                      <li>
+                          <a href="<?= base_url('all-projects') ?>"><i class="bi bi-arrow-right-short"></i>All Projects </a>
+                      </li>
+                      <li>
+                          <a href="<?= base_url('project-category') ?>"><i class="bi bi-arrow-right-short"></i>Project Category</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- mannage  Equipment end here-->
+          </ul>
+          <!--end navigation-->
+      </aside>
+      <!--end sidebar -->
+  <?php
+    }
+    ?>
